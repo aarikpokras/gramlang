e14d2d11ef09034fb7a9b1a9cb6127e4c2bafec6
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-12-17 12:41:08 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 if [[ "$(ls)" =~ "CMakeLists.txt" ]] && [[ "$(basename $PWD)" == "gramlang" ]]; then
-  cmake -Bbuild
-  cd build
-  make
-  mv gramlang ..
-  cd ..
-  echo You may want to run the clean.sh script now.
-  echo Build successful!
+  if [ "$CI" = true ]; then
+    echo Welcome, GitHub CI!
+    cmake -Bbuild
+    cd build
+    make
+    mv gramlang ..
+    cd ..
+  else
+    cmake -Bbuild
+    cd build
+    make
+    mv gramlang ..
+    cd ..
+    echo You may want to run the clean.sh script now.
+    echo Build successful!
   exit 0
 else
   echo Are you in the gramlang directory?
