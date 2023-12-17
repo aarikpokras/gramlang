f2e9052926b8c8bc939a92e3820875857d2fb55a
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-12-17 12:42:08 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ if [[ "$(ls)" =~ "CMakeLists.txt" ]] && [[ "$(basename $PWD)" == "gramlang" ]]; 
     cd ..
     echo You may want to run the clean.sh script now.
     echo Build successful!
+  fi
   exit 0
 else
   echo Are you in the gramlang directory?
